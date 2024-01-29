75fda29d881d1e4f0e3907af8d1c87fcf99e7ca7
riteshivankar <149803980+riteshivankar@users.noreply.github.com>
2024-01-29 14:29:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Square() {
+    var numInput = document.getElementById("numInput");
+    var inputValue = numInput.value
+    var square = (inputValue * inputValue)
+    var square1 = document.getElementById("square1")
+    square1.value = square
+
+
+}
